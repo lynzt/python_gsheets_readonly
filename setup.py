0035a3471bcd2dc46cabfbc38a3809be55874c95
lindsay <techells22@gmail.com>
2017-10-22 22:23:38 -0500

    fix setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
-setup(name='python_g_sheets',
-    version='0.0.1',
+setup(name='python_gsheets_readonly',
+    version='0.0.2',
     description='read google sheets',
     author='lynzt',
     url='https://github.com/lynzt/python_gsheets_readonly',
